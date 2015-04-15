bcea0abd4b0e320cdaf42a4d82013f61ed67adce
Alec Olesky <aolesky@gmail.com>
2015-04-15 01:19:18 -0400

    geofence creation

--- DutyReporting/app/src/main/java/com/example/swords/dutyreporting/LoggedInActivity.java
+++ DutyReporting/app/src/main/java/com/example/swords/dutyreporting/LoggedInActivity.java
@@ -1,7 +1,9 @@
 package com.example.swords.dutyreporting;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -10,10 +12,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
-
-
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class LoggedInActivity extends ActionBarActivity
@@ -21,6 +26,8 @@ public class LoggedInActivity extends ActionBarActivity
     private String username;
     private GoogleApiClient mGoogleApiClient;
     private boolean succesfulConnection;
+    private ArrayList<Geofence> mGeofenceList = new ArrayList<Geofence>();
+    private PendingIntent mGeofencePendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,58 @@ public class LoggedInActivity extends ActionBarActivity
         username = intent.getStringExtra("USERNAME");
         succesfulConnection = false;
         buildGoogleApiClient();
+
+        setupGeofences();
+    }
+
+    protected void setupGeofences() {
+        Resources res = getResources();
+        String[] resLocations = res.getStringArray(R.array.location_array);
+        String radius = res.getString(R.string.radius);
+
+        List<FenceLocation> locations = new ArrayList<FenceLocation>();
+
+        for (int i = 0; i < resLocations.length; i++) {
+            locations.add(new FenceLocation(resLocations[i]));
+        }
+
+        for (FenceLocation loc : locations) {
+            mGeofenceList.add(new Geofence.Builder()
+                    // Set the request ID of the geofence. This is a string to identify this
+                    // geofence.
+                    .setRequestId(loc.getName())
+
+                    .setCircularRegion(
+                            loc.getLatitude(),
+                            loc.getLongitude(),
+                            1000
+                    )
+                    .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                            Geofence.GEOFENCE_TRANSITION_EXIT)
+                    .build());
+        }
+
+    }
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mGeofencePendingIntent != null) {
+            return mGeofencePendingIntent;
+        }
+//        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+//        return PendingIntent.getService(this, 0, intent, PendingIntent.
+//                FLAG_UPDATE_CURRENT);
+        return null;
     }
 
 
